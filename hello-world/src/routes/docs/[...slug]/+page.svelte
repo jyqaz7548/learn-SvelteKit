54b18d88b21d39206f90e672d0b9b440b7da7cf3
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-11-07 19:40:08 +0900

    Create +page.svelte

---
+++ hello-world/src/routes/docs/[...slug]/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import {page} from '$app/stores';
+    console.log($page.params.slug)
+
+    const slugArr = $page.params.slug.split('/');
+    console.log(slugArr);
+</script>
+
+<h1>Docs 페이지</h1>
+
+{#if slugArr.length === 1}
+<h1>Docs 페이지; 주제 {slugArr[0]}</h1>
+{:else if slugArr.length === 2}
+<h1>Docs 페이지; {slugArr[0]} ; 소주제 {slugArr[1]}</h1>
+{/if}
